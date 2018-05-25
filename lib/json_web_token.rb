70e116369e1f920c69cdd9490595ce7b4b469e49
jfcaballero <jfcp@inesingenieros.com>
2018-05-25 20:14:31 +0200

    add algorithm: 'HS256'

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -3,11 +3,11 @@ class JsonWebToken
    def encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
     # binding.pry
-    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
    end
 
    def decode(token)
-     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
+     body = JWT.decode(token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' })[0]
      HashWithIndifferentAccess.new body
    rescue
      nil
