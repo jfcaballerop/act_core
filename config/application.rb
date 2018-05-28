86f0f7b911fed38922cb5a681b1f25ba7fadb916
jfcp <jfcp@inesingenieros.com>
2018-05-28 16:08:38 +0200

    add verbs http to cors filter

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module SgaCore
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
       end
     end   
     # Only loads a smaller set of middleware suitable for API only apps.
